3578be8765ccb1a052f4e8e102bd8f16ffcc851d
Romuald Brillout <github.com@brillout.com>
2019-10-21 16:58:27 +0200

    wip

--- server/frontend.js
+++ server/frontend.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const server = express();
-const buildPath = __dirname+'/../dist/';
+const buildPath = __dirname+'/../frontend/dist/';
 server.use(express.static(buildPath))
 
 module.exports = server;
